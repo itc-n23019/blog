f95b5e8dd4a30c486791ee51f8ed07f2c54ce587
itc-n23019 <n23019@std.it-college.ac.jp>
2023-12-14 06:46:24 +0000

    Add about.js and index2.js

---
+++ pages/index2.js
@@ -0,0 +1,15 @@
+const Home = () => {
+  return (
+    <>
+      <header>HEADER</header>
+      <main>
+        <div>
+          <h1>CUBE</h1>
+          <p>アウトプットするサイト</p>
+        </div>
+      </main>
+      <footer>FOOTER</footer>
+    </>
+  )
+}
+export default Home
