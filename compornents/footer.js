f26165657bac1c9f2032e2da8299ef0b42db6fdc
itc-n23019 <n23019@std.it-college.ac.jp>
2023-12-20 05:38:09 +0000

    4.4

--- compornents/footer.js
+++ compornents/footer.js
@@ -1,6 +1,14 @@
 import Logo from 'compornents/logo'
+import styles from 'styles/footer.module.css'
 
 const Footer = () => {
-  return <footer><Logo /></footer>
+  return (
+    <footer className={styles.wrapper}>
+      <div className={styles.flexcontainer}>
+        <Logo />
+        [ソーシャル]
+      </div>
+    </footer>
+  )
 }
 export default Footer
